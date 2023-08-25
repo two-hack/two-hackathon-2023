5a4240ee660ee28d6afa07e85cacd8cdf8f31bba
Brian <bhar5201@uni.sydney.edu.au>
2023-08-26 07:57:51 +1000

    in progress: formatting the initial prompt

--- script.py
+++ script.py
@@ -32,101 +32,38 @@
 
 
 import requests
+from key import KEY
+import userinfo
+
 
 CONV = []
 
-<<<<<<< HEAD
+def make_initial_prompt():
+    LANGUAGE = "Chinese" # the one to learn
+    # PERSONAL_INFO = (1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
+    personal_info = userinfo.get_user_personal_details()
+    user_proficiency = userinfo.get_user_language_proficiency()
 
-#INITIALISATION PROMPTS
-setup = """
-Act like a conversation language facilitator for the language "{}".
-Converse with the user to learn more concepts and uses of the language and to rate objectively the proficiency of the user in the language.
+    f = open("security.txt")
+    SECURITY = f.read().format(LANGUAGE)
+    f.close()
 
-The criterion for the rating is:
+    f = open("criterion.txt")
+    CRITERION = f.read()
+    f.close()
 
-Vocabulary:
-Basic Phrases: Greetings, farewells, and common expressions.
-Everyday Items: Names of foods, clothing, and household items.
-Work-Related Terms: Industry-specific jargon or terminology.
-Emotions and Feelings: Words to express different moods and sentiments.
+    f = open("personal.txt")
+    PERSONAL = f.read().format(1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
+    f.close()
 
-Grammar:
-Sentence Structure: Understanding the basic structure of sentences in the target language.
-Tenses: Past, present, and future tenses for verbs.
-Pronouns: Subject, object, and possessive pronouns.
-Modifiers: Adjectives, adverbs, and other words that modify nouns and verbs.
+    f = open("convo.txt")
+    CONVO = f.read()
+    f.close()
 
-Social and Cultural Context:
-Formality Levels: Knowing when to use formal or informal language.
-Idioms and Slang: Phrases that are specific to certain cultures or groups.
-Social Norms: Understanding the do's and don'ts in different social settings.
-Cultural References: Popular sayings, historical events, or cultural phenomena that are often mentioned in conversation.
-
-Comprehension:
-Context Clues: Using surrounding information to understand unfamiliar words or phrases.
-Relevance: Relevance of responses of the user according to the topic of conversation.
-
-Conversational Skills:
-Questioning: Asking questions to gather information or keep the conversation going.
-Active Listening: Showing interest and understanding while the other person is speaking.
-  
-In a scale from 1 to 100 this are the current ratings and understandings of the user based on previous conversations using this same program:
-Vocabulary: "{}"
-Grammar: "{}"
-Social and cultural context: "{}"
-Comprehension: "{}"
-Conversational skills: "{}"
-
-Some behaviours to note about this users general use of the language and how can they improve:
-""
-
-Some common mistakes that user makes in the language:
-""
-
-Some conversations that were already had:
-""
-
-Personal information of this user:
-Name: "{}"
-Gender: "{}"
-Interests:"{}"
-Age: "{}"
-Personality: "{}"
-
-Create an interesting conversation with the user in the language specified before considering all of the information given.
-Keep the conversion simple enough so that the user will understand and follow the conversation, but challenging enogh that there is always learning oportunities and mistakes corrections in the conversation. 
-
-After giving a glossary of words that could be used for this conversation: Considering all of the users, inculidng level of proficiency,  information please create a glossary of all of the potential words that might be useful for this conversation. 
-write them in this format:
-Word, Definition, Translation, Example_Sentence. 
-
-After that start with a question that could spark an interesting conversation.
-""".format("Chinese",1,1,1,1,1,"Tony","Male", "Programming, Guitar", 18, "Talkative")
-=======
-LANGUAGE = "Chinese" # the one to learn
-# PERSONAL_INFO = (1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
->>>>>>> 64c9bfe3909a484e4371966cc39ccd3508708e92
-
-
-f = open("security.txt")
-SECURITY = f.read().format(LANGUAGE)
-f.close()
-
-f = open("criterion.txt")
-CRITERION = f.read()
-f.close()
-
-f = open("personal.txt")
-PERSONAL = f.read().format(1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
-f.close()
-
-f = open("convo.txt")
-CONVO = f.read()
-f.close()
-
-f = open("final_prompt.txt")
-FINAL = f.read()
-f.close()
+    f = open("final_prompt.txt")
+    FINAL = f.read()
+    f.close()
+    return (SECURITY, CRITERION, PERSONAL, CONV, FINAL)
 
 
 def chat_with_gpt(prompt):
@@ -137,7 +74,7 @@ def chat_with_gpt(prompt):
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"  # Replace with your actual API key
+        "Authorization": "Bearer " + KEY  # Replace with your actual API key
     }
     data = {
         "messages": CONV,
@@ -159,6 +96,7 @@ def chat_with_gpt(prompt):
     return assistant_reply
 
 def init():
+    SECURITY, CRITERION, PERSONAL, CONVO = make_initial_prompt()
     chat_with_gpt(SECURITY)
     chat_with_gpt(CRITERION)
     chat_with_gpt(PERSONAL)
