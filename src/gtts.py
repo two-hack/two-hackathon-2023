2533d98ab34e8dc6d5dd8c15f8063473cec8ea34
shionjjl <143237303+shionjjl@users.noreply.github.com>
2023-08-27 07:18:29 +1000

    google cloud text to speech function has been completed
    use as such:
    
    import gtts
    gtts.synthesise(str: text, str: language, str: gender)
    
    then output.mp3 will be saved in the current working directory

--- src/gtts.py
+++ src/gtts.py
@@ -8,11 +8,36 @@ from google.cloud import texttospeech
 
 import os
 
-os.system('export GOOGLE_APPLICATION_CREDENTIALS="steam-treat-397115-d9246f5f8e7e.json"')
-os.system('set GOOGLE_APPLICATION_CREDENTIALS="steam-treat-397115-d9246f5f8e7e.json"')
+#os.system('export GOOGLE_APPLICATION_CREDENTIALS="steam-treat-397115-d9246f5f8e7e.json"')
+#os.system('set GOOGLE_APPLICATION_CREDENTIALS="steam-treat-397115-d9246f5f8e7e.json"')
 
-language_codes = {
-    "english" : ""
+language_voices = {
+    "arabic" : ["ar-XA", "ar-XA-Wavenet-A", "ar-XA-Wavenet-C"],
+    "bengali" : ["bn-IN", "bn-IN-Wavenet-A", "bn-IN-Wavenet-B"],
+    "cantonese" : ["yue-HK", "yue-HK-Standard-A", "yue-HK-Standard-B"],
+    "czech" : ["cs-CZ", "cs-CZ-Wavenet-A", "cs-CZ-Wavenet-A"], # no male voice for czech
+    "danish" : ["da-DK", "da-DK-Neural2-D", "da-DK-Wavenet-C"],
+    "dutch" : ["nl-BE", "nl-BE-Wavenet-A", "nl-BE-Wavenet-B"],
+    "english" : ["en-AU", "en-AU-Neural2-C", "en-AU-Neural2-B"],
+    "filipino" : ["fil-PH", "fil-ph-Neural2-A", "fil-ph-Neural2-D"],
+    "french" : ["fr-FR", "fr-FR-Neural2-A", "fr-FR-Neural2-B"],
+    "german" : ["de-DE", "de-DE-Neural2-C", "de-DE-Neural2-B"],
+    "hindi" : ["hi-IN", "hi-IN-Neural2-D", "hi-IN-Neural2-C"],
+    "indonesian" : ["id-ID", "id-ID-Wavenet-D", "id-ID-Wavenet-B"],
+    "italian" : ["it-IT", "it-IT-Neural2-A", "it-IT-Neural2-C"],
+    "japanese" : ["ja-JP", "ja-JP-Neural2-B", "ja-JP-Neural2-D"],
+    "korea" : ["ko-KR", "ko-KR-Neural2-A", "ko-KR-Neural2-C"],
+    "mandarin" : ["cmn-CN", "cmn-CN-Wavenet-A", "cmn-CN-Wavenet-B"],
+    "portugese" : ["pt-BR", "pt-BR-Neural2-A", "pt-BR-Neural2-B"],
+    "punjabi" : ["pa-IN", "pa-IN-Wavenet-A", "pa-IN-Wavenet-D"],
+    "russian" : ["ru-RU", "ru-RU-Wavenet-C", "ru-RU-Wavenet-B"],
+    "spanish" : ["es-US", "es-US-Neural2-A", "es-US-Neural2-C"],
+    "swedish" : ["sv-SE", "sv-SE-Wavenet-A", "sv-SE-Wavenet-C"],
+    "tamil" : ["ta-IN", "ta-IN-Wavenet-A", "ta-IN-Wavenet-D"],
+    "thai" : ["th-TH", "th-TH-Neural2-C", "th-TH-Neural2-C"],   # no male voice for thai
+    "turkish" : ["tr-TR", "tr-TR-Wavenet-D", "tr-TR-Wavenet-E"],
+    "ukrainian" : ["uk-UA", "uk-UA-Wavenet-A", "uk-UA-Wavenet-A"], # no male voice for ukrainian
+    "vietnamese" : ["vi-VN", "vi-VN-Neural2-A", "vi-VN-Neural2-D"]
 }
 
 def synthesise(input, language, gender):
@@ -25,8 +50,14 @@ def synthesise(input, language, gender):
 
     # Build the voice request, select the language code ("en-US") and the ssml
     # voice gender ("neutral")
+    if gender.lower() == 'male':
+        ssml_gender = texttospeech.SsmlVoiceGender.MALE
+        name = language_voices[language.lower()][2]
+    else:
+        ssml_gender = texttospeech.SsmlVoiceGender.FEMALE
+        language_voices[language.lower()][1]
     voice = texttospeech.VoiceSelectionParams(
-        language_code="ja-JP", name="ja-JP-Neural2-C", ssml_gender=texttospeech.SsmlVoiceGender.MALE
+        language_code=language_voices[language.lower()][0], name=name, ssml_gender=ssml_gender
     )
 
     # Select the type of audio file you want returned
@@ -45,3 +76,27 @@ def synthesise(input, language, gender):
         # Write the response to the output file.
         out.write(response.audio_content)
         print('Audio content written to file "output.mp3"')
+
+def list_voices():
+    """Lists the available voices."""
+
+    client = texttospeech.TextToSpeechClient()
+
+    # Performs the list voices request
+    voices = client.list_voices()
+
+    for voice in voices.voices:
+        # Display the voice's name. Example: tpc-vocoded
+        print(f"Name: {voice.name}")
+
+        # Display the supported language codes for this voice. Example: "en-US"
+        for language_code in voice.language_codes:
+            print(f"Supported language: {language_code}")
+
+        ssml_gender = texttospeech.SsmlVoiceGender(voice.ssml_gender)
+
+        # Display the SSML Voice Gender
+        print(f"SSML Voice Gender: {ssml_gender.name}")
+
+        # Display the natural sample rate hertz for this voice. Example: 24000
+        print(f"Natural Sample Rate Hertz: {voice.natural_sample_rate_hertz}\n")
