82313875bc963dc5439d290ef362c38fdd0da955
Tony <52038141+tonyjwt@users.noreply.github.com>
2023-08-26 17:40:38 +1000

    Update script.py
    get rid of all tex in main script

--- script.py
+++ script.py
@@ -1,90 +1,63 @@
+# import requests
+
+# def chat_with_gpt(prompt):
+#     url = "https://api.openai.com/v1/chat/completions"
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": "Bearer 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"  # Replace with your actual API key
+#     }
+#     data = {
+#         "messages": [
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": prompt}
+#         ]
+#     }
+
+#     response = requests.post(url, headers=headers, json=data)
+#     response_json = response.json()
+#     assistant_reply = response_json["choices"][0]["message"]["content"]
+    
+#     return assistant_reply
+
+# if __name__ == "__main__":
+#     print("Welcome to the ChatGPT Conversation!")
+#     while True:
+#         user_input = input("You: ")
+#         if user_input.lower() == "exit":
+#             print("Goodbye!")
+#             break
+#         assistant_response = chat_with_gpt(user_input)
+#         print("Assistant:", assistant_response)
+
+
 
 import requests
 
 CONV = []
 
-#INITIALISATION PROMPTS
-setup = """
-Act like a conversation language facilitator for the language "{}".
-Converse with the user to learn more concepts and uses of the language and to rate objectively the proficiency of the user in the language.
-
-The criterion for the rating is:
-
-Vocabulary:
-Basic Phrases: Greetings, farewells, and common expressions.
-Everyday Items: Names of foods, clothing, and household items.
-Work-Related Terms: Industry-specific jargon or terminology.
-Emotions and Feelings: Words to express different moods and sentiments.
-
-Grammar:
-Sentence Structure: Understanding the basic structure of sentences in the target language.
-Tenses: Past, present, and future tenses for verbs.
-Pronouns: Subject, object, and possessive pronouns.
-Modifiers: Adjectives, adverbs, and other words that modify nouns and verbs.
-
-Social and Cultural Context:
-Formality Levels: Knowing when to use formal or informal language.
-Idioms and Slang: Phrases that are specific to certain cultures or groups.
-Social Norms: Understanding the do's and don'ts in different social settings.
-Cultural References: Popular sayings, historical events, or cultural phenomena that are often mentioned in conversation.
-
-Comprehension:
-Context Clues: Using surrounding information to understand unfamiliar words or phrases.
-Relevance: Relevance of responses of the user according to the topic of conversation.
-
-Conversational Skills:
-Questioning: Asking questions to gather information or keep the conversation going.
-Active Listening: Showing interest and understanding while the other person is speaking.
-  
-In a scale from 1 to 100 this are the current ratings and understandings of the user based on previous conversations using this same program:
-Vocabulary: "{}"
-Grammar: "{}"
-Social and cultural context: "{}"
-Comprehension: "{}"
-Conversational skills: "{}"
-
-Some behaviours to note about this users general use of the language and how can they improve:
-""
-
-Some common mistakes that user makes in the language:
-""
-
-Some conversations that were already had:
-""
-
-Personal information of this user:
-Name: "{}"
-Gender: "{}"
-Interests:"{}"
-Age: "{}"
-Personality: "{}"
-
-Create an interesting conversation with the user in the language specified before considering all of the information given.
-Keep the conversion simple enough so that the user will understand and follow the conversation, but challenging enogh that there is always learning oportunities and mistakes corrections in the conversation. 
-
-After giving a glossary of words that could be used for this conversation: Considering all of the users, inculidng level of proficiency,  information please create a glossary of all of the potential words that might be useful for this conversation. 
-write them in this format:
-Word, Definition, Translation, Example_Sentence. 
-
-After that start with a question that could spark an interesting conversation.
-""".format("Chinese",1,1,1,1,1,"Tony","Male", "Programming, Guitar", 18, "Talkative")
+LANGUAGE = "Chinese" # the one to learn
+# PERSONAL_INFO = (1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
 
 
-saveData = """
-Based on the criteria that was given on the first prompt rate the performance of this user from 1 to 100 in the following format: Criteria, rating
+f = open("security.txt")
+SECURITY = f.read().format(LANGUAGE)
+f.close()
 
-Write thing to note about this users behaviours that might help the next conversation be more interesting and educative in a few sentences.
+f = open("criterion.txt")
+CRITERION = f.read()
+f.close()
 
-Write about how the users personality presented on this conversation and use the personality from the first prompt to update the personality field, describe it in 10 words.
+f = open("personal.txt")
+PERSONAL = f.read().format(1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
+f.close()
 
-Give some feedback to the user that will encourage them to keep learning while also outlining ways they can improve in each criteria and in general.
+f = open("convo.txt")
+CONVO = f.read()
+f.close()
 
-Write some common mistakes in the language that this user has.
-
-Summarise in less than 15 words the topics of conversation.
-
-Write all of these separated by newlines and no text or answers other that was specified.
-"""
+f = open("final_prompt.txt")
+FINAL = f.read()
+f.close()
 
 
 def chat_with_gpt(prompt):
@@ -95,7 +68,7 @@ def chat_with_gpt(prompt):
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"  # Replace with your actual API key
+        "Authorization": "Bearer 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"  # Replace with your actual API key
     }
     data = {
         "messages": CONV,
@@ -114,17 +87,16 @@ def chat_with_gpt(prompt):
     #CONV.append(response_json["message"][-1])
     CONV.append({"role": "assistant", "content": assistant_reply})
 
-
     return assistant_reply
 
 def init():
-    print(chat_with_gpt(setup))
-
+    chat_with_gpt(SECURITY)
+    chat_with_gpt(CRITERION)
+    chat_with_gpt(PERSONAL)
+    print(chat_with_gpt(CONVO))
 
 def end():
-    print(chat_with_gpt(saveData))
-
-
+    print(chat_with_gpt(FINAL))
 
 if __name__ == "__main__":
     print("Welcome to the language app!")
