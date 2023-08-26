8a9cebcdfefeff3596bce9069432ef8bcae97b25
Tony <52038141+tonyjwt@users.noreply.github.com>
2023-08-26 12:32:35 +0000

    Updates

--- script.py
+++ script.py
@@ -1,90 +1,69 @@
-
 import requests
+import matplotlib.pyplot as plt
+import datetime
+import pickle
+import os
+from key import KEY # local file
+
+USER_ID = 500
+FILENAME = str(USER_ID) + ".pkl"
+
+class DataStorage:
+    def __init__(self):
+        self.data = {}
+
+    def add_entry(self, timestamp, values):
+        self.data[timestamp] = values
+
+    def save_data(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self.data, f)
+
+    def load_data(self, filename):
+        with open(filename, 'rb') as f:
+            self.data = pickle.load(f)
+
+    def getKeys(self):
+        return self.data.keys()
+
+    def getValues(self):
+        return self.data.values()
+
+datastorage = DataStorage()
+
+if os.path.exists(FILENAME):
+    datastorage.load_data(FILENAME)
+
+# graph = {}
 
 CONV = []
 
-#INITIALISATION PROMPTS
-setup = """
-Act like a conversation language facilitator for the language "{}".
-Converse with the user to learn more concepts and uses of the language and to rate objectively the proficiency of the user in the language.
+LANGUAGE = "Chinese" # the one to learn
+# PERSONAL_INFO = (1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
 
-The criterion for the rating is:
+f = open("security.txt")
+SECURITY = f.read().format(LANGUAGE)
+f.close()
 
-Vocabulary:
-Basic Phrases: Greetings, farewells, and common expressions.
-Everyday Items: Names of foods, clothing, and household items.
-Work-Related Terms: Industry-specific jargon or terminology.
-Emotions and Feelings: Words to express different moods and sentiments.
+f = open("criterion.txt")
+CRITERION = f.read()
+f.close()
 
-Grammar:
-Sentence Structure: Understanding the basic structure of sentences in the target language.
-Tenses: Past, present, and future tenses for verbs.
-Pronouns: Subject, object, and possessive pronouns.
-Modifiers: Adjectives, adverbs, and other words that modify nouns and verbs.
+f = open("personal.txt")
+PERSONAL = f.read().format(1,1,1,1,1, "Complete beginner", "Grammatical errors", "None", "John", "Male", "Guitar, programming, AFL", "45", "Outgoing")
+f.close()
 
-Social and Cultural Context:
-Formality Levels: Knowing when to use formal or informal language.
-Idioms and Slang: Phrases that are specific to certain cultures or groups.
-Social Norms: Understanding the do's and don'ts in different social settings.
-Cultural References: Popular sayings, historical events, or cultural phenomena that are often mentioned in conversation.
+f = open("convo.txt")
+CONVO = f.read()
+f.close()
 
-Comprehension:
-Context Clues: Using surrounding information to understand unfamiliar words or phrases.
-Relevance: Relevance of responses of the user according to the topic of conversation.
+f = open("final_prompt.txt")
+FINAL = f.read()
+f.close()
 
-Conversational Skills:
-Questioning: Asking questions to gather information or keep the conversation going.
-Active Listening: Showing interest and understanding while the other person is speaking.
-  
-In a scale from 1 to 100 this are the current ratings and understandings of the user based on previous conversations using this same program:
-Vocabulary: "{}"
-Grammar: "{}"
-Social and cultural context: "{}"
-Comprehension: "{}"
-Conversational skills: "{}"
-
-Some behaviours to note about this users general use of the language and how can they improve:
-""
-
-Some common mistakes that user makes in the language:
-""
-
-Some conversations that were already had:
-""
-
-Personal information of this user:
-Name: "{}"
-Gender: "{}"
-Interests:"{}"
-Age: "{}"
-Personality: "{}"
-
-Create an interesting conversation with the user in the language specified before considering all of the information given.
-Keep the conversion simple enough so that the user will understand and follow the conversation, but challenging enogh that there is always learning oportunities and mistakes corrections in the conversation. 
-
-After giving a glossary of words that could be used for this conversation: Considering all of the users, inculidng level of proficiency,  information please create a glossary of all of the potential words that might be useful for this conversation. 
-write them in this format:
-Word, Definition, Translation, Example_Sentence. 
-
-After that start with a question that could spark an interesting conversation.
-""".format("Chinese",1,1,1,1,1,"Tony","Male", "Programming, Guitar", 18, "Talkative")
-
-
-saveData = """
-Based on the criteria that was given on the first prompt rate the performance of this user from 1 to 100 in the following format: Criteria, rating
-
-Write thing to note about this users behaviours that might help the next conversation be more interesting and educative in a few sentences.
-
-Write about how the users personality presented on this conversation and use the personality from the first prompt to update the personality field, describe it in 10 words.
-
-Give some feedback to the user that will encourage them to keep learning while also outlining ways they can improve in each criteria and in general.
-
-Write some common mistakes in the language that this user has.
-
-Summarise in less than 15 words the topics of conversation.
-
-Write all of these separated by newlines and no text or answers other that was specified.
-"""
+f = open("explain.txt")
+EXPLAIN = f.read()
+f.close()
 
 
 def chat_with_gpt(prompt):
@@ -95,7 +74,7 @@ def chat_with_gpt(prompt):
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"  # Replace with your actual API key
+        "Authorization": "Bearer {}".format(KEY) 
     }
     data = {
         "messages": CONV,
@@ -114,17 +93,99 @@ def chat_with_gpt(prompt):
     #CONV.append(response_json["message"][-1])
     CONV.append({"role": "assistant", "content": assistant_reply})
 
-
     return assistant_reply
 
 def init():
-    print(chat_with_gpt(setup))
+    chat_with_gpt(SECURITY)
+    chat_with_gpt(CRITERION)
+    chat_with_gpt(PERSONAL)
+    print(chat_with_gpt(CONVO))
 
+def end(lastInput):
+    summary = chat_with_gpt(FINAL)
+    print(summary)
 
-def end():
-    print(chat_with_gpt(saveData))
+    lines = []
 
+    for line in summary.split("\n"):
+        if line.strip():
+            lines.append(line)
 
+    try:
+        statsLines = lines[:5]
+    except:
+        statsLines = []
+
+    stats = []
+    for statsLine in statsLines:
+        try:
+            num = int(statsLine.split(",")[1].strip())
+        except:
+            num = 1
+        stats.append(num)
+
+    date = str(datetime.datetime.now().date())
+    time = str(datetime.datetime.now().time().replace(microsecond=0))
+    timestamp = date + " " + time
+
+    datastorage.add_entry(timestamp, stats)
+    
+    try:
+        BEHAVIOUR = lines[5]
+    except:
+        BEHAVIOUR = ""
+
+    try:
+        PERSONALITY = lines[6]
+    except:
+        PERSONALITY = ""
+
+    try:
+        MISTAKES = lines[8]
+    except:
+        MISTAKES = ""
+
+    if lastInput == "graph":
+
+        # Extract timestamps and values from the dictionary
+        # graph = {
+        #     '2023-08-26 20:59:48': [25, 25, 15, 30, 30],
+        #     '2023-08-26 21:59:48': [30, 27, 16, 30, 50],
+        #     '2023-08-26 22:59:48': [40, 30, 20, 40, 70],
+        #     '2023-08-26 23:59:00': [61, 35, 54, 41, 95]
+        # }
+
+        timestamps = list(datastorage.getKeys())
+        values = list(datastorage.getValues())
+
+        # Transpose the values to separate the 5 elements into different lists
+        transposed_values = list(zip(*values))
+
+        # Labels for the line graphs
+        labels = [
+            "Vocabulary",
+            "Grammar",
+            "Social & Cultural Context",
+            "Comprehension",
+            "Conversational Skills"
+        ]
+
+        # Create a line plot for each element with appropriate label
+        for i, value_list in enumerate(transposed_values):
+            plt.plot(timestamps, value_list, marker='o', label=labels[i])
+
+        plt.xlabel("Timestamps")
+        plt.ylabel("Values")
+        plt.title("User {}'s Progress".format(USER_ID))
+        plt.legend()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        plt.savefig("User {}s Progress.png".format(USER_ID))
+
+        # print(graph)
+
+    datastorage.save_data(FILENAME)
 
 if __name__ == "__main__":
     print("Welcome to the language app!")
@@ -133,11 +194,18 @@ if __name__ == "__main__":
 
     while True:
         user_input = input("You: ")
+        if user_input.lower() == "explain":
+            user_input = EXPLAIN
         if user_input.lower() == "end conversation":
-            
             break
+        if user_input.lower() == "graph":
+            break
+
         assistant_response = chat_with_gpt(user_input + " . ADMIN Further instructions (do not mention these in conversation): keep in mind the rules stated in first prompt, only provide feedback in English, provide romanization for non-English characters. Don't provide the summary of my stats and progress until I say 'end conversation'")
         print("Assistant:", assistant_response)
 
-    end()
+    end(user_input.lower())
+    
+
+
 
